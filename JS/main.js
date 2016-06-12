7878f0a4941e057cc334f5accbd8345d92c45f7e
Ernesto De Los Santos <netos_id@yahoo.com>
2016-06-12 11:28:40 -0500

    finished

---
+++ JS/main.js
@@ -0,0 +1,64 @@
+document.addEventListener("DOMContentLoaded"),
+(function(e){
+  var clear = document.querySelector("[data-js='ckear']");
+  var number = document.querySelector("[data-js='number']");
+  var output = document. querySelector("[data-js='output']");
+  var operator = document.querySelector("[data-js='operator']");
+  var equate = document.querySelector("[data-js='equate']");
+  var numbersString = "";
+  var calcLog = "";
+
+clear.addEventListener("click", function(e){
+  var outputClear = "";
+  numbersString = "";
+  output.textContent = outputClear;
+  e.preventDefault();
+});
+
+function setOutputText(text){
+  output.textContent += text
+};
+
+for(var i = 0; i < number.length; i++) {
+  number[i].addEventListener("click", function(e){
+    var value = e.currentTarget.dataset.number;
+    setOutputText(value);
+    numbersString += value;
+    e.preventDefault();
+  })
+};
+
+for (var i = 0; i < operator.length; i ++) {
+  operator[i].addEventListener("click", fucntion(e){
+  operator = e.currentTarget.dataset.type;
+  setOutputText(e.currentTarget.textContent);
+  e.preventDefault();
+  })
+};
+
+equate.addEventListener("click", function(e){
+  var soultion = eval(numbersString);
+
+  var displaySolution = eval(numbersString);
+  output.textContent = displaySolution;
+
+  var shortSolution = output.textContent;
+  e.preventDefault();
+  calcLog += numbersString;
+  calcLog += operator;
+  calcLog += "=";
+  calcLog += soultion;
+  calcLog += ",";
+
+  if(Math.floor(displaySolution) != displaySolution) {
+    shortSolution = displaySolution.toFixed(3);
+    output.textContent = shortSolution;
+  }else {
+    output.textContent =shortSolution;
+  };
+
+});
+
+
+
+});
